ac8eaacd44c91b124f38f2f9fb8da8e969a2205f
Romitou <romitou@protonmail.com>
2021-02-08 00:00:47 +0100

    :goal_net: Better PufferException

--- src/main/java/fr/romitou/puffer4j/PufferException.java
+++ src/main/java/fr/romitou/puffer4j/PufferException.java
@@ -1,14 +1,20 @@
 package fr.romitou.puffer4j;
 
+import retrofit2.Response;
+
 import java.io.IOException;
 
 public class PufferException extends Exception {
 
-    public PufferException(String message, IOException e) {
-        super(message + e.getMessage());
-    }
-
     public PufferException(String message) {
         super(message);
+    }
+
+    public PufferException(String message, IOException e) {
+        super("An error occurred while " + message + ":" + e);
+    }
+
+    public <T> PufferException(Response<T> response) {
+        super("Request on " + response.raw().request().url() + " was not successful and returned " + HttpResponse.valueOf("HTTP_" + response.code()).label + " HTTP code.");
     }
 }
