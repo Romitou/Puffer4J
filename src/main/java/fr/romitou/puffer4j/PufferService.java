463b42b188354e2205fd6b3e7c226ca73958fe27
Romitou <romitou@protonmail.com>
2021-02-08 00:01:02 +0100

    :sparkles: Adding more endpoints to PufferService

--- src/main/java/fr/romitou/puffer4j/PufferService.java
+++ src/main/java/fr/romitou/puffer4j/PufferService.java
@@ -1,21 +1,53 @@
 package fr.romitou.puffer4j;
 
-import fr.romitou.puffer4j.objects.AuthRequest;
-import fr.romitou.puffer4j.objects.PufferNode;
-import fr.romitou.puffer4j.objects.PufferSession;
+import fr.romitou.puffer4j.requests.PufferAuth;
+import fr.romitou.puffer4j.responses.*;
 import retrofit2.Call;
-import retrofit2.http.Body;
-import retrofit2.http.GET;
-import retrofit2.http.POST;
+import retrofit2.http.*;
 
 import java.util.List;
 
 public interface PufferService {
 
-    @POST("/auth/login")
-    Call<PufferSession> getSession(@Body AuthRequest authRequest);
+    // -- Authentication --
 
-    @GET("/nodes")
+    @POST("/auth/login")
+    Call<PufferSession> getSession(@Body PufferAuth pufferAuth);
+
+    @GET("/api/self")
+    Call<PufferUser> getSelf();
+
+    @POST("/api/self")
+    Call<PufferUser> updateSelf(@Body PufferUser pufferUser);
+
+
+    // -- Nodes --
+
+    @GET("/api/nodes")
     Call<List<PufferNode>> getNodes();
+
+    @GET("/api/nodes/{id}")
+    Call<PufferNode> getNode(@Path("id") String nodeId);
+
+    @POST("/api/nodes")
+    Call<PufferNode> createNode(@Body PufferNode pufferNode);
+
+    @PUT("/api/nodes/{id}")
+    Call<Void> editNode(@Path("id") String nodeId, @Body PufferNode pufferNode);
+
+    @DELETE("/api/nodes/{id}")
+    Call<Void> deleteNode(@Path("id") String nodeId);
+
+    @GET("/api/nodes/{id}/deploy")
+    Call<PufferDeploy> getNodeDeploy(@Path("id") String nodeId);
+
+
+    // -- Servers --
+
+    @GET("/api/servers")
+    Call<List<PufferServer>> getServers();
+
+    @GET("/api/servers/{id}")
+    Call<PufferServer> getServer(@Path("id") String id);
 
 }
