604ea752f7ef0956475d0b6a1052d2288399bf20
Romitou <romitou@protonmail.com>
2021-02-07 23:59:35 +0100

    :sparkles: Make AuthRequest more clear by PufferAuth

---
+++ src/main/java/fr/romitou/puffer4j/requests/PufferAuth.java
@@ -0,0 +1,19 @@
+package fr.romitou.puffer4j.requests;
+
+public class PufferAuth {
+
+    private String email;
+    private String password;
+
+    /**
+     * Create a authorization object for PufferPanel in order to retrieve user session.
+     *
+     * @param email The user's email address
+     * @param password The user's password
+     */
+    public PufferAuth(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+}
